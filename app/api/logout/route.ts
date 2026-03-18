fb50b7f36ebc6143becd0a2e0e481ba321d3fdb9
Renzo Costarelli <renzo.costarelli@gmail.com>
2026-03-18 14:33:15 -0300

    logout test

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,3 +1,5 @@
+import { SITE_URL } from "@/lib/constants";
+
 export async function GET(request: Request) {
-  return Response.redirect(new URL("/ingresar", request.url), 302);
+  return Response.redirect(new URL(`${SITE_URL}/ingresar`, request.url), 302);
 }
